743375457c6024d79a520b135aa3b7fcfd69bfcd
Patel <patelnirav9033@gmail.com>
2022-07-19 17:53:41 +0530

    create web test for all api

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MovieController extends AbstractController
 {
     /**
-     * @Route("/api/v1/movies", name="add_movie")
+     * @Route("/api/v1/movies", name="add_movie", methods={"POST"})
      *
      *
      * @param \Symfony\Component\HttpFoundation\Request $request
@@ -42,29 +42,32 @@ class MovieController extends AbstractController
     }
 
     /**
-     * @Route("/api/v1/movies/{id}", name="get_movie")
+     * @Route("/api/v1/movies/{id}", name="get_movie", methods={"GET"})
      *
-     * @param Movies $movies
-     *
+     * @param Request $request
+     * @param ManagerRegistry $doctrine
      * @return JsonResponse
-     *
-     * @ParamConverter("movies", options={"id" = "id"})
      */
-    public function getAction(Movies $movies): JsonResponse
+    public function getAction(Request $request,  ManagerRegistry $doctrine): JsonResponse
     {
-        $movieArr = array(
-            'name' => $movies->getName(),
-            'casts' => $movies->getCasts(),
-            'release_data' => $movies->getReleaseAt()->format('d-m-y'),
-            'director' => $movies->getDirector(),
-            'ratings' => $movies->getRatings()
-        );
-
+        $movieArr = [];
+        $id = $request->get('id');
+        $em = $doctrine->getManager();
+        $movies = $em->getRepository(Movies::class)->find($id);
+        if ($movies instanceof Movies) {
+            $movieArr = array(
+                'name' => $movies->getName(),
+                'casts' => $movies->getCasts(),
+                'release_data' => $movies->getReleaseAt()->format('d-m-y'),
+                'director' => $movies->getDirector(),
+                'ratings' => $movies->getRatings()
+            );
+        }
         return new JsonResponse($movieArr);
     }
 
     /**
-     * @Route("/api/v1/list/movies", name="list_movie")
+     * @Route("/api/v1/list/movies", name="list_movie", methods={"GET"})
      *
      * @param \Symfony\Component\HttpFoundation\Request $request
      * @param MovieService $movieService
--- tests/Controller/MovieControllerTest.php
+++ tests/Controller/MovieControllerTest.php
@@ -3,11 +3,33 @@
 namespace App\Tests\Controller;
 
 use App\Entity\Movies;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MovieControllerTest extends WebTestCase
 {
     protected $url = 'http://127.0.0.1:8000';
+
+    /**
+     * @return array|int|string|bool
+     */
+    public function testGenerateRandomUser()
+    {
+        $container = self::getContainer();
+        $em = $container->get('doctrine')->getManager();
+        $user = new User();
+        $user->setEmail('test_'.rand(1,100).'@gmail.com');
+        $user->setFirstName('test_'.rand(1,100));
+        $user->setLastName('test_'.rand(1,100));
+        try {
+            $em->persist($user);
+            $em->flush();
+            $this->assertTrue(true, 'successfully created user');
+        } catch (\Exception $exception) {
+            dump($exception);
+        }
+    }
+
     /**
      * @return void
      */
@@ -15,99 +37,84 @@ class MovieControllerTest extends WebTestCase
     {
         $client = static::createClient();
         $createUrl = $this->url.'/api/v1/movies';
-
         $objMovies = new Movies();
-        $objMovies->setName('test '.rand(1,100));
-        $objMovies->setDirector('director '.rand(1,100));
+        $userId = $this->getRandomUserId();
+        if (!$userId > 0) $userId = 1;
+        $objMovies->setName('test_'.rand(1,100));
+        $objMovies->setDirector('director_'.rand(1,100));
         $objMovies->setReleaseAt(new \DateTime());
         $objMovies->setRatings([]);
         $objMovies->setCasts([]);
 
         $movies = array(
-        'name' => '',
-        'director' => '',
-        'release_date' => '',
-        'ratings' => '',
-        'casts' => ''
-    );
-        $request = $client->post('/api/programmers', null, json_encode($movies));
-        $response = $request->send();
-    print_r($response);exit();
-        $objMovies = $client->request('GET', $createUrl);
-        $crawler = $client->request('POST', $createUrl, array(
-            'movies' => array(
-                'name' => '',
-                'director' => '',
-                'release_date' => '',
-                'ratings' => '',
-                'casts' => ''
-            )),
-            array(),
-            array(
-                'HTTP_X-Requested-With' => 'XMLHttpRequest',
-            )
+            'name' => '',
+            'director' => '',
+            'release_date' => '',
+            'ratings' => '',
+            'casts' => ''
         );
-
-        if(302 == $client->getResponse()->getStatusCode())
-        {
-            $this->fail('colleague already registered.');
-        }
-        $this->assertTrue(true,'successfully registered new colleague');
+        $newUrl = $createUrl.'?name='.$objMovies->getName().'&director='.$objMovies->getDirector().'&release_date='.$objMovies->getReleaseAt()->format('d-m-y').'&user_id='.$userId;
+        $crawler = $client->request('POST', $newUrl);
+        $this->assertTrue(true,'successfully created new movie');
     }
+
     /**
      * @return void
      */
-   /* public function testUpdate(): void
+   public function testGetMovie(): void
     {
         $client = static::createClient();
-        $container = self::getContainer();
-        $em = $container->get('doctrine')->getManager();
-        $repository = $container->get('doctrine')->getRepository(Colleague::class);
-        $randomColleague = $this->getRandomColleague();
-        $colleagueUpdate = $repository->findOneById($randomColleague);
-        $updateUrl = $this->url.'/'.$randomColleague.'/edit';
-        $colleagueUpdate->setName('test Update'.rand(1,100));
-        $colleagueUpdate->setEmail('test'.rand(1,100).'Update@gmail.com');
-        $colleagueUpdate->setNotes('implemented by Web Test Update');
-        $crawler = $client->request('GET', $updateUrl);
-        $token = $crawler->filter('[name="colleague[_token]"]')->attr("value");
-        $crawler = $client->request('POST', $updateUrl, array(
-            'colleague' => array(
-                '_token' => $token,
-                'name' => $colleagueUpdate->getName(),
-                'email' => $colleagueUpdate->getEmail(),
-                'notes' => $colleagueUpdate->getNotes()
-            )),
-            array(),
-            array(
-                'HTTP_X-Requested-With' => 'XMLHttpRequest',
-            )
-        );
-        $this->assertTrue(303 == $client->getResponse()->getStatusCode(),'successfully Updated colleague');
-    }*/
+        $getUrl = $this->url.'/api/v1/movies/';
+        $randomId = $this->getRandomMovieId();
+        if (!$randomId > 0) $randomId = 1;
+        $newUrl = $getUrl.$randomId;
+        $crawler = $client->request('GET', $newUrl);
+        dump("response => ".$client->getResponse()->getContent());
+        $this->assertTrue(true);
+    }
+
     /**
      * @return void
      */
-   /* public function testSendGreetings()
+   public function testGetUserMovies()
     {
         $client = static::createClient();
-        $randomColleague = $this->getRandomColleague();
-        $sendMailUrl = $this->url.'/send-greetings-email/'.$randomColleague;
-        $crawler = $client->request('POST', $sendMailUrl );
-        $this->assertTrue(303 == $client->getResponse()->getStatusCode(), "successfully send greetings!");
-    }*/
+        $getUrl = $this->url.'/api/v1/list/movies?';
+        $userId = $this->getRandomUserId();
+        if (!$userId > 0) $userId = 1;
+        $newUrl = $getUrl.'user_id='.$userId;
+        $crawler = $client->request('GET', $newUrl);
+        dump("response => ".$client->getResponse()->getContent());
+        $this->assertTrue(true);
+    }
+
     /**
      * @return array|int|string
      */
-    /*public function getRandomColleague()
+    public function getRandomMovieId()
     {
         $container = self::getContainer();
-        $repository = $container->get('doctrine')->getRepository(Colleague::class);
-        $colleagues = $repository->findAll();
+        $repository = $container->get('doctrine')->getRepository(Movies::class);
+        $movies = $repository->findAll();
         $id = array();
-        foreach ($colleagues as $colleague) {
-            $id[] = $colleague->getId();
+        foreach ($movies as $movie) {
+            $id[] = $movie->getId();
         }
         return array_rand($id);
-    }*/
+    }
+
+    /**
+     * @return array|int|string|bool
+     */
+    public function getRandomUserId()
+    {
+        $container = self::getContainer();
+        $repository = $container->get('doctrine')->getRepository(User::class);
+        $users = $repository->findAll();
+        $id = array();
+        foreach ($users as $user) {
+            $id[] = $user->getId();
+        }
+        return array_rand($id);
+    }
 }
